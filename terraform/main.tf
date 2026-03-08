747149f7fce25a89a101e902e2245e16d80d7781
Subin Biju <subinbiju176@gmail.com>
2026-03-08 20:42:52 +0530

    Update storage container to use storage_account_id

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,8 +16,8 @@ resource "azurerm_storage_account" "tfstate" {
 }
 
 resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.tfstate.name
+  name                 = "tfstate"
+  storage_account_id   = azurerm_storage_account.tfstate.id
   container_access_type = "private"
 }
 
