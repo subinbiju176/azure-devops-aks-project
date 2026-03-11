4fc3254bc1da4f0304941194a45e24dba4ddc365
Subin Biju <subinbiju176@gmail.com>
2026-03-11 14:27:29 +0530

    Trigger CI/CD pipeline test

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "DevOps project running on Azure!"
+    return "DevOps Pipeline Version 2 Running!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
