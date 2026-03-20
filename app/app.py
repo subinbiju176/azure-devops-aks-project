fecfd45c40f9956d8d1609ba8b5a3aec2218deb4
Subin Biju <subinbiju176@gmail.com>
2026-03-20 17:59:09 +0530

    Final CI/CD pipeline execution

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "DevOps Pipeline Version 2 Running!"
+    return "DevOps Pipeline Final Version"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
